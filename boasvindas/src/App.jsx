175d7dc369ddb12592ca4a050c02bd79215a403c
Vk-programator <victorcandidoalves@gmail.com>
2025-04-24 11:05:40 -0300

    24/4

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,28 @@
+import BoasVindas from "./Exemplo1"
+
+function App()
+{
+
+return (
+<div>
+
+<h1>Boas vindas ao react com JavaScript</h1>
+
+<p>
+
+  Este arquivo App.js que será o nosso ponto de partida para o desenvolvimento
+  de aplicação React utilizando JavaScript
+
+</p>
+  <BoasVindas nome="Victor" />
+  <BoasVindas nome="João" />
+  <BoasVindas nome="Miguel" />
+  <BoasVindas nome="Vina" />
+  <BoasVindas nome="Danilo" />
+</div>
+
+)
+
+}
+
+export default App
