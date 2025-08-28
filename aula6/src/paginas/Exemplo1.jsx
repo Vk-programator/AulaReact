4e470278b2fc4cec8c72b235695f730bf98ac809
Vk-programator <victorcandidoalves@gmail.com>
2025-08-28 09:08:28 -0300

    28-08f

--- aula6/src/paginas/Exemplo1.jsx
+++ aula6/src/paginas/Exemplo1.jsx
@@ -1,27 +1,51 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-export default function Exemplo1(){
-        const [nome, setNome] = useState("Victor");
-        const [idade, setIdade] = useState(19);
-    return(
+export default function Exemplo1() {
+    const [nome, setNome] = useState("Victor");
+    const [idade, setIdade] = useState(19);
+    const [resultado, setResultado] = useState(null);
+
+    function calcular() {
+        let dias = Number(idade) * 365;
+        if (nome != "" && idade != "") {
+            setResultado("Olá " + nome + " você ja viveu " + dias + "dias");
+        } else {
+            setResultado("Não foram informados valores suficientes");
+        }
+    }
+    function limpar() {
+        setIdade("");
+        setNome("");
+        setResultado("")
+    }
+
+    return (
         <div>
             <h1>Exemplo 1</h1>
             <div className="container">
-            <form>
-                <p>
-                    Digite o nome do aluno <br />
-                    <input type="text" value={nome}/>
-                </p>
-                <p>
-                    Digite a idade do aluno <br />
-                    <input type="text" value={idade}/>
-                </p>
-                <p>
-                    <input type="button" value="Exemplo1" />
-                    <input type="button" value="Limpar" />
-                </p>
-            </form>
-            <li><Link to={"/"}>voltar</Link></li>
+                <form>
+                    <p>
+                        Digite o nome do aluno <br />
+                        <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
+                    </p>
+                    <p>
+                        Digite a idade do aluno <br />
+                        <input type="text" value={idade} onChange={(e) => setIdade(e.target.value)} />
+                    </p>
+                    <p>
+                        <input type="button" value="Exemplo1" onClick={calcular} />
+                        <input type="button" value="Limpar" onClick={limpar} />
+                    </p>
+                    <p>
+                        <b>Resultados:</b><br />
+                        Nome:{nome} <br />
+                        Idade:{idade} <br />
+                        <p>
+                            {resultado}
+                        </p>
+                    </p>
+                </form>
+                <li><Link to={"/"}>voltar</Link></li>
             </div>
         </div>
     )
--- aula6/src/paginas/Exemplo2.jsx
+++ aula6/src/paginas/Exemplo2.jsx
@@ -1,8 +1,37 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 export default function Exemplo2(){
+    const [n1, setN1] = useState(0);
+    const [n2, setN2] = useState(0);
+    const [resultado, setResultado]= useState(null);
+
+    function calcular(){
+        let soma = Number(n1) + Number(n2)
+        setResultado("A soma de " + n1 + " + " + n2 + " = " + soma)
+    }
     return(
+
         <div>
-            <li><Link to={"/"}>voltar</Link></li>
+            <h1>Exemplo2</h1>
+            <div className="container">
+                <form>
+                    <p>
+                        Numero 1:
+                        <input type="text" value={n1} onChange={(e) => setN1(e.target.value)} />
+                    </p>
+                    <p>
+                        Numero 1:
+                        <input type="text" value={n2} onChange={(e) => setN2(e.target.value)} />
+                    </p>
+                    <p>
+                        <input type="button" value="Exemplo2"onClick={calcular} />
+                    </p>
+                    <p>
+                        {resultado}
+                    </p>
+                </form>
+            </div>
         </div>
     )
 }
