f4227d235d998b4f55a22158d2fe401214b1da90
Vk-programator <victorcandidoalves@gmail.com>
2025-10-09 09:16:09 -0300

    09-10

--- aulaBD/servidor/index.js
+++ aulaBD/servidor/index.js
@@ -29,3 +29,41 @@ banco.connect( (erro) => {
 app.listen(port, () => {
     console.log("Servidor rodando em http//localhost:" + port);
 });
+
+app.get("/aluno", (req, res) =>{
+    const sql = "SELECT * from aluno";
+
+    banco.query(sql,(erro,resultados) => {
+        if (erro){
+            console.log(erro);
+            return res.status(200).json(resultados);
+        } else {
+            console.log(resultados);
+            return res.status(200).json(resultados);
+        }
+
+
+    });
+});
+
+app.get("/aluno/codigo", (req,res) => {
+    const {codigo} = req.params;
+
+    const sql = "SELECT * from aluno WHERE codigo = ?";
+
+    banco.query(sql, [codigo], (erro, resultados) => {
+
+        if (erro){
+            console.log(erro);
+            return res.status(500).json({error : "Erro ao consulta aluno"});
+        }
+        if (resultados.length === 0 ) {
+
+            return res.status(400).json({message: "Aluno não encontrado"});
+        }
+
+        return res.status(200).json(resultados[0]);
+    });
+   
+});
+
